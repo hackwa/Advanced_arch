be4a43315f1727917977ef0dab4a463b84252961
Anurag Dubey <anurag.dubey@colorado.edu>
2015-10-27 10:08:33 -0600

    update fles

--- project_2/source/mod3/Sources/main.c
+++ project_2/source/mod3/Sources/main.c
@@ -36,6 +36,7 @@
 #include "DbgCs1.h"
 #include "adConv1.h"
 #include "fsl_spi_hal.h"
+#include "fsl_tpm_hal.h"
 #if CPU_INIT_CONFIG
   #include "Init_Config.h"
 #endif
@@ -80,7 +81,75 @@ extern volatile uint16_t SwTimerIsrCounter = 0U;
 // Variables
 ///////////////////////////////////////////////////////////////////////////////
 struct SPI_type *lcd;
+struct TPM_type *flowTPM;
+struct TPM_type *frequencyTPM;
+tpm_pwm_param_t *pwmControl;
 
+
+long getFrequency(){
+	 char *buf[1000];
+	    int i = 0;
+	    int c = 0;
+	    long binary_data[1000];
+	    FILE * data = fopen("ECEN5003Data7.txt","r");
+	    for(i;i<1000;i++)
+	    {
+	        buf[i] = (char*) calloc(5,sizeof(char));
+	        fgets(buf[i],sizeof(buf[i]),data);
+	        binary_data[i] = strtol(buf[i],0,16);
+	//        printf("%d\n",binary_data[i] );
+	    }
+	    long max = binary_data[0];
+	    for ( i=0; i<1000; i++ )
+	    {
+	    	if (binary_data[i] > max)
+	    	{max = binary_data[i];}
+	    }
+	    int array[1000],x=0;
+	    for (c;c<1000;c++)
+	    {
+	    	if (binary_data[c]==max)
+	    	{
+	    		array[x] = c;x++;
+	 //           printf("Xrray: %d\n", c);
+	    	}
+	    }
+	    int t = 2*(array[1]-array[0]);
+	    long f = pow(10,4);
+	    f = f/t;
+//	    UART_direct_msg("Frequency %lu\n",f );
+	    return f;
+}
+
+float getFlow(long f, int32_t temp){
+    float d,v0,v,pid,a,tol = 10,b,density,viscosity, area;
+    d = 0.0127;
+    pid =0.07366;
+    v0=1;
+    float n = temp-3.9863;
+    float area1 = 3.142*pow((double)d,2);
+    area = area1/4;
+    a = 508929.2*(temp+68.12963);
+    b = 247.8/(296.15-140);
+    float y = (temp+288.9414)/a;
+    density = 1000*(1-(y*pow(n,2)));
+    viscosity = 2.4*pow(10,-5)*10*b;
+    while(tol>0.01)
+    {
+    	float re = (density*v0*pid)/viscosity;
+    	float pth = 1.036/sqrt(re);
+    	float st = 0.2684 - pth;
+    	v = f*d/st;
+    	tol = (abs(v0-v)/v0)*100;
+    	v0 = v;
+    }
+  //  UART_msg_put("Velocity: %f\n",v0 );
+    float vFeetPerSec, flow;
+    vFeetPerSec = v0 * 3.28;
+    pid = 2.9;
+    flow = 2.45 * pid * pid * vFeetPerSec;
+    return flow;
+}
 ///////////////////////////////////////////////////////////////////////////////
 // Main
 ///////////////////////////////////////////////////////////////////////////////
@@ -99,6 +168,11 @@ int main(void)
     int32_t conversionCompleted = 0;
     uint32_t updateBoundariesCounter = 0;
     int32_t tempArray[UPDATE_BOUNDARIES_TIME * 2];
+    long freq;
+    float flowrate;
+    uint32_t frequencyTPMChannel = 4, freqmax = 2000;
+    uint32_t flowTPMChannel = 3, dutyCycle;
+    uint32_t flowmax = 1000;
 
 // initialize serial buffer pointers
    rx_in_ptr =  rx_buf; /* pointer to the receive in data */
@@ -164,13 +238,19 @@ int main(void)
     }
 
 //  ECEN 5003 add code as indicated
-
+freq = getFrequency();
 //  calculate flow()
-
-//  4-20 output ()    // use TMP0 channel 3  proporional rate to flow
-
-//  Pulse output()   // use TMP0 channel 4  propotional rate to frequency
-
+flowrate = getFlow(freq,currentTemperature);
+pwmControl->uFrequencyHZ = 8000000;
+//  4-20 output ()    // use TPM0 channel 3  proporional rate to flow
+dutyCycle = (flowmax / flowrate) * 100;
+pwmControl->uDutyCyclePercent = dutyCycle;
+TPM_HAL_EnablePwmMode(frequencyTPM,pwmControl,flowTPMChannel);
+//  Pulse output()   // use TPM0 channel 4  propotional rate to frequency
+dutyCycle = (freqmax / freq) * 100;
+pwmControl->uDutyCyclePercent = dutyCycle;
+TPM_HAL_EnablePwmMode(flowTPM,pwmControl,frequencyTPMChannel);
+//TPM_HAL_EnablePwmMode
 //  LCD_Display()   // use the SPI port to send flow number
     uint32_t data;
     SPI_HAL_WriteData(lcd,data);
